916414d499219fa850161c7a6bd087c9df061067
aatxaerandio <75436856+aatxaerandio@users.noreply.github.com>
2024-06-18 11:29:45 +0200

    Update Checkpoint_8_Exercises.js

--- Full-Stack/module_04_javascript/Checkpoints_7_&_8/Checkpoint_8_Exercises.js
+++ Full-Stack/module_04_javascript/Checkpoints_7_&_8/Checkpoint_8_Exercises.js
@@ -1,1 +1,59 @@
+/* EJERCICIOS PRÁCTICOS JAVASCRIPT */
 
+
+
+/*Ejercicio 1
+Cree un bucle for en JS que imprima cada nombre en esta lista. 
+miLista = “velma”, “exploradora”, “jane”, “john”, “harry”.*/
+
+let milista = ["velma", "exploradora", "jane", "john", "harry"]
+
+for(let counter = 0; counter < milista.length; counter++){
+  console.log(milista[counter]);
+}
+
+/*
+velma
+exploradora
+jane
+john
+harry
+*/
+
+
+
+
+
+/*Ejercicio 2
+Cree un bucle while que recorra la misma lista y también imprima los nombres.
+Nota: Recuerda crear un contador para que el ciclo no sea infinito.*/
+
+let milista = ["velma", "exploradora", "jane", "john", "harry"]
+
+counter = 0;
+while(counter < milista.length){
+  console.log(milista[counter]);
+  counter++;
+}
+
+/*
+velma
+exploradora
+jane
+john
+harry
+*/
+
+
+
+
+
+/*Ejercicio 3
+Cree una función de flecha que devuelva "Hola mundo".*/
+
+const saludos = () => {
+    console.log('Hola mundo');
+  }
+  
+
+  saludos()  // Hola mundo
